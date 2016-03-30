b851fea3e5e53d18a8dfd017346a0e7124224781
Michael Eason <michaelseason2009@yahoo.com>
2016-03-29 20:13:23 -0500

    added a spinner

--- app/src/main/java/jsm/utlotlocater/permitselector.java
+++ app/src/main/java/jsm/utlotlocater/permitselector.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class permitselector extends AppCompatActivity {
 
@@ -16,8 +18,16 @@ public class permitselector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permitselector);
-
         Log.d(TAG, "in onCreate");
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.permit_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
     @Override
@@ -25,6 +35,12 @@ public class permitselector extends AppCompatActivity {
         super.onPause();
         Log.d(TAG, "in onPause");
     }//end onPause override method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "in onResume");
+    }//end onResume override method
 
     @Override
     protected void onStop() {
